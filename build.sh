6261f4407ce889c16bc0b83deb58505860256375
Elio Campitelli <eliocampitelli@gmail.com>
2025-12-11 12:45:30 +1100

    Adds section title to competing interests and etc...

--- build.sh
+++ build.sh
@@ -4,24 +4,6 @@ dir=analysis/paper
 source=$dir/access-ice.qmd
 zip=access-ice.zip
 
-# main=$dir/main.qmd 
-# supp=$dir/supp.qmd 
-
-
-# cp $source $main
-# quarto render $main --profile main
-# cp $dir/main.aux $dir/ref_main.aux
-
-# cp $source $supp
-# quarto render $supp --profile supp
-# cp $dir/supp.aux $dir/ref_supp.aux
-
-# rm $main $supp 
-
-
-# pdflatex main.tex
-# pdflatex supp.tex
-
 quarto render $source
 
 cd $dir
